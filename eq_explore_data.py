19fd55f7da85f0a67e3f7abbf293ee4365fb4689
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-27 02:53:05 -0700

    Created a scattergeo map of earthquakes using JSON

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -1,6 +1,6 @@
 import json
-
-# Explore the structure of the data
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
 
 filename = 'data/eq_data_1_day_m1.json'
 
@@ -8,8 +8,26 @@ with open(filename) as f:
 	#json.load converts the data into a giant dictionary
 	all_eq_data = json.load(f)
 
-# create a more readable file
-readable_file = 'data/readable_eq_data.json'
-with open(readable_file, 'w') as f:
-	# json.dump takes a json data object and file and writes the data to the file
-	json.dump(all_eq_data, f, indent=4)
+all_eq_dicts = all_eq_data['features']
+
+mags,lons,lats = [],[],[]
+for eq_dict in all_eq_dicts:
+	mag= eq_dict['properties']['mag']
+	lon = eq_dict['geometry']['coordinates'][0]
+	lat = eq_dict['geometry']['coordinates'][1]
+	mags.append(mag)
+	lons.append(lon)
+	lats.append(lat)
+
+# Map the earthquakes
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig,filename='global_earthquakes.html')
+
+# # create a more readable file
+# readable_file = 'data/readable_eq_data.json'
+# with open(readable_file, 'w') as f:
+# 	# json.dump takes a json data object and file and writes the data to the file
+# 	json.dump(all_eq_data, f, indent=4)
