32a973c809f602ca6b81d07f2445e5f601a7490c
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-27 02:38:38 -0700

    Create a readable file of JSON data

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -1,1 +1,15 @@
 import json
+
+# Explore the structure of the data
+
+filename = 'data/eq_data_1_day_m1.json'
+
+with open(filename) as f:
+	#json.load converts the data into a giant dictionary
+	all_eq_data = json.load(f)
+
+# create a more readable file
+readable_file = 'data/readable_eq_data.json'
+with open(readable_file, 'w') as f:
+	# json.dump takes a json data object and file and writes the data to the file
+	json.dump(all_eq_data, f, indent=4)
